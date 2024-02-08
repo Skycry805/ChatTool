78b4fbc5a3ed2a8d40327912734aee1081eabfc6
Niclas Sieveneck <niclass95@live.de>
2024-02-08 18:34:56 +0100

    New structure for docker

--- backend/api.py
+++ backend/api.py
@@ -2,13 +2,28 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
+import sys
+from flasgger import Swagger
 
 chat = Flask(__name__)
+swagger = Swagger(chat)
 CORS(chat)
-
 
 @chat.route('/send_message_to_server', methods=['POST'])
 def receive_message():
+    """
+    Test mit viel SWAG, Tööörnup
+    ---
+    parameters:
+      - name: name
+        in: path
+        type: json
+        required: true
+        description: TEEST 123.
+    responses:
+        200:
+           description: TEST undso....
+    """
 
     json_data = request.get_json()
 
@@ -63,4 +78,12 @@ def send_message():
     return jsonify(response), 200
 
 if __name__ == '__main__':
-    chat.run(host='0.0.0.0', port=5000,debug=True)
+    # Disable debug, when deployed in a container, for production use
+    debug = False
+    if len( sys.argv ) > 1:
+        first_arg = str(sys.argv[1])
+        if first_arg.lower() == "debug":
+            debug=True
+            print("Debug enabled!")
+
+    chat.run(host='0.0.0.0', port=5000,debug=debug)
--- docker/build_testing.sh
+++ docker/build_testing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo TEST-Script
+echo TEST-Script Build Testing
 docker-compose down
 docker-compose build
 docker-compose up -d
